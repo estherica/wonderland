ddca223cb4b22755dd366ad5ad47fbc2c35f2cb7
estherica <belleshamharoth@gmail.com>
2020-08-05 15:24:03 +0300

    numbers and mathematics

--- Lesson1/targil2.py
+++ Lesson1/targil2.py
@@ -6,12 +6,17 @@ print("Alafim: " + str(a))
 b=num//100
 print("Meot: " + str(b-a*10))
 
+print("Meot: " + str((num%1000)//100))
+
 c=num//10
 print("Asarot: " + str(c-b*10))
 
-d=num
-print("Ahadot:" + str(d-c*10))
+print("Asarot: " + str((num%100)//10))
 
+d=num
+print("Ahadot: " + str(d-c*10))
+
+print("Ahadot: " + str(num%10))
 
 '''
 Alafim=4
