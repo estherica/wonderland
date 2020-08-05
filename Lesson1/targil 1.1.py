a7a7a4ee3dfdfaf1037991b424fd532afd50a478
estherica <belleshamharoth@gmail.com>
2020-08-05 15:04:12 +0300

    numbers and mathematics

---
+++ Lesson1/targil 1.1.py
@@ -0,0 +1,28 @@
+a=20
+b=11.56
+c="estherica"
+
+#This is Integer
+print(a)
+print (type(a))
+
+#This is Float
+print(b)
+print (type(b))
+
+#This is String
+print(c)
+print (type(c))
+
+print("Hello world\nHow're you?")
+
+name="estherica"
+age=32
+
+print("Hello, " +name + "\nHow're you?" + "\nYour age is " +str(age))
+
+mail=input("Enter your mail: ")
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+
+print("\nFull name: " + name + "\nYour age is " + str(age) + "\nYour email is " + mail)
---
+++ Lesson1/targil2.py
@@ -0,0 +1,21 @@
+num=4567
+
+a=num//1000
+print("Alafim: " + str(a))
+
+b=num//100
+print("Meot: " + str(b-a*10))
+
+c=num//10
+print("Asarot: " + str(c-b*10))
+
+d=num
+print("Ahadot:" + str(d-c*10))
+
+
+'''
+Alafim=4
+Meot=5
+Asarot=6
+Ahadot=7
+'''
