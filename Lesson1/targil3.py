b07e5ea47d06d1b7c3c6fb0c07f9583cfcb78d37
estherica <belleshamharoth@gmail.com>
2020-08-05 18:45:29 +0300

    letters

---
+++ Lesson1/targil3.py
@@ -0,0 +1,26 @@
+a="estherica"
+b=32
+c="belleshamharoth"
+print("Full name: estherica \nMy age: 32 \nMy nickname: belleshamharoth")
+print("Full name: " + a + "\nMy age: " + str(b+2) + "\nMy nickname: " + c)
+
+d='''
+My name is Estherica
+I love art
+Follow me on instagram esthericas
+'''
+print(d)
+
+print("p1={},p2={},p3={},p4={red}".format(1, 1.0, "estherica", red='BBFD'))
+print("estherica" in "estherica surkis")
+a="estherica surkis"
+print("estherica" in a)
+
+s1="JERUSALEM"
+print(s1)
+print(s1[0])
+print(s1[-1])
+print(s1[2:-4])
+print(s1[3:-3])
+print(s1[: :2])
+print(s1[::-1])
