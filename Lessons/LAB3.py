24609a3a0efd0b773e5b9d89798a69b2f957115d
estherica <belleshamharoth@gmail.com>
2020-08-05 18:58:20 +0300

    labs

---
+++ Lessons/LAB3.py
@@ -0,0 +1,7 @@
+a="Esther Surkis"
+b="belleshamharoth@gmail.com"
+c=int(input("Please, enter your age: "))
+
+print("Full name: " + a + "\nE-mail: " + b + "\nAge: " + str(c))
+print(a[::-1])
+print(c*3)
