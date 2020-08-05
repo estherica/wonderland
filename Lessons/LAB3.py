530cddc37c6f0f8f708c9b16e8cff8dfe350533c
estherica <belleshamharoth@gmail.com>
2020-08-05 19:04:02 +0300

    labs

--- Lessons/LAB3.py
+++ Lessons/LAB3.py
@@ -1,7 +1,7 @@
-a="Esther Surkis"
+a="esther surkis"
 b="belleshamharoth@gmail.com"
 c=int(input("Please, enter your age: "))
 
 print("Full name: " + a + "\nE-mail: " + b + "\nAge: " + str(c))
-print(a[::-1])
-print(c*3)
+print("Full name: " + str(a[::-1]) + "\nE-mail: " + b + "\nAge: " + str(c*3))
+print("esther" in "idan ben dudu yuval shimon yael gal adam shahar yana")
