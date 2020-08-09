c7c2ff5f717fbbb9cc85b9acd0260079140f73b0
estherica <belleshamharoth@gmail.com>
2020-08-09 17:50:32 +0300

    labs

--- Lessons/LAB6.py
+++ Lessons/LAB6.py
@@ -9,16 +9,16 @@ elif(a==2):
     new_list.append(input("Insert your 2nd IP: \n"))
     new_list.append(input("Insert your 3rd IP: \n"))
     new_list.append(input("Insert your 4th IP: \n"))
-    print("Your Ips: " + str(new_list))
+    print("Your Ips:\n---------\n" + str(new_list))
 elif(a==3):
     my_dict = {}
-    my_dict.update({"www.google.com": input("Please, enter your 1st DNS IP address...")})
-    my_dict.update({"www.google2.com": input("Please, enter your 2nd DNS IP address...")})
-    my_dict.update({"www.google3.com": input("Please, enter your 3rd DNS IP address...")})
-    my_dict.update({"www.google4.com": input("Please, enter your 4th DNS IP address...")})
-    print("Your DNS Ips: " + str(my_dict))
+    my_dict.update({input("Please enter your URL: "): input("Please, enter your 1st DNS IP address...")})
+    my_dict.update({input("Please enter your URL: "): input("Please, enter your 2nd DNS IP address...")})
+    my_dict.update({input("Please enter your URL: "): input("Please, enter your 3rd DNS IP address...")})
+    my_dict.update({input("Please enter your URL: "): input("Please, enter your 4th DNS IP address...")})
+    print("Your DNS Ips:\n------------\n" + str(my_dict))
 elif(a==4):
-    b = input("Let's play a game! Please insert a word! \n")
+    b = input("Let's play a game! Please enter a word! \n")
     c = (b[::-1])
     if (c == b):
         print("This is a Polindrom!")
