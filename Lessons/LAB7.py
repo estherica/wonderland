e39abf34296822230e8519de0ed4910eb7c27cdb
estherica <belleshamharoth@gmail.com>
2020-08-13 19:20:36 +0300

    labs

--- Lessons/LAB7.py
+++ Lessons/LAB7.py
@@ -1,26 +1,29 @@
 print("Welcome to our Lottery!\n" + "-----------------------\n" + "One roulette spin costs 3 NIS.\nThe number of attempts is now limited.\n")
 a=int(input("Please, make a payment!..\n"))
-print("You have {0} attempts. Good luck!".format(str(a//3)))
-print("Your change is... " + str(a-(a//3*3)) + " NIS. Enjoy your game!\n" + "-----------------------------------------")
+turns=(a//3)
+print("You have {0} attempts. Good luck!".format(str(turns)))
+print("Your change is... " + str(a%3) + " NIS. Enjoy your game!\n" + "-----------------------------------------")
 
 from random import randint
+from time import sleep
 print("Let's play...\n")
-import time
-time.sleep(2)
-num1=(randint(1,6))
-num2=(randint(1,6))
-print("1st number: " + str(num1) + "\n2nd number: " + str(num2) + "\n")
+sleep(2)
+total=0
 
-if num1==num2:
-    print("Congratulations! You won 100 NIS.")
-elif num1==num2==6:
-    print("Congratulations! You won 1000 NIS.")
-elif num2==2!=num1:
-    print("Congratulations! You won 40 NIS.")
-elif num1==1!=num2:
-    print("Congratulations! You won 20 NIS.")
-else:
-    print("Well, maybe next time...")
+for i in range(turns):
+    print("Round # " + str(i+1) + "\n--------")
+    num1=(randint(1,6))
+    num2=(randint(1,6))
+    print("1st number: " + str(num1) + "\n2nd number: " + str(num2) + "\n")
 
-time.sleep(2)
-print("\nThank you for joining us tonight! We are looking forward seeing you again!")
+    if num1==num2:
+        total=total+100
+    elif num1==6==num2:
+        total=total+1000
+    elif num2==2!=num1:
+        total=total+40
+    elif num1==1!=num2:
+        total=total+20
+
+sleep(2)
+print("Congratulations! You won  " + str(total) + "NIS" + "\nThank you for joining us tonight! \nWe are looking forward seeing you again!")
