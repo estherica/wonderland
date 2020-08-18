4b19752d5cb78afdd8031456f143f9d63b3182aa
estherica <belleshamharoth@gmail.com>
2020-08-18 03:52:38 +0300

    exercises

--- Projects/draft.py
+++
@@ -1,9 +0,0 @@
-def cubes(num):
-    sum = 0
-    for i in range(1,num):
-        sum=sum+(i**3)
-    print("The sum of the cubes is " + str(sum))
-    return sum
-
-num=int(input("Please, enter an integer... "))
-print(cubes(num))
