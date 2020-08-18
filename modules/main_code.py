1608783cb0e566ec8a8941a4a69aa03cfb351f33
estherica <belleshamharoth@gmail.com>
2020-08-18 04:17:56 +0300

    exercises

---
+++ modules/main_code.py
@@ -0,0 +1,9 @@
+from random import randint
+from time import sleep
+from modules.defim import menu,calculating,dogs_age
+
+menu()
+sleep(3)
+print("wow!\n\n")
+calculating(3,6)
+dogs_age(int(input("Enter dog's age")))
